e6a5af2ab825350674266c63fdc13f012e757423
Kiara Kaiser <kiara.kaiser@jku.at>
2020-01-22 13:21:09 +0100

    added board and model

---
+++ src/main/java/assignments20/q1/sudokuweb/Board.java
@@ -0,0 +1,11 @@
+package assignments20.q1.sudokuweb;
+
+public class Board implements Model {
+
+
+    @Override
+    public Boolean checkSudoku(Sudoku sudoku) {
+
+        return true;
+    }
+}
---
+++ src/main/java/assignments20/q1/sudokuweb/Model.java
@@ -0,0 +1,7 @@
+package assignments20.q1.sudokuweb;
+
+public interface Model {
+
+    Boolean checkSudoku(Sudoku sudoku);
+
+}
